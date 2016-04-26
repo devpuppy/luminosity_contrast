a2b440726b82b4ecd64680eb75dbd4b0979259f9
Justin Jones <justin@devpuppy.com>
2016-04-26 18:37:16 -0400

    LuminosityContrast.ratio

--- lib/luminosity_contrast.rb
+++ lib/luminosity_contrast.rb
@@ -2,11 +2,24 @@ require "luminosity_contrast/version"
 
 module LuminosityContrast
 
+  # module methods
   class << self
-    def relative_luminance(r,g,b)
+    # expects two RGB values as Arrays of three numbers
+    # example: [0, 0, 0], [255.0, 255.0, 255.0]
+    # returns the Luminosity Contrast Ratio between 1.0 and 21.0
+    def ratio(rgb1, rgb2)
+      l1, l2 = [rgb1, rgb2].map { |rgb| relative_luminance(*rgb) }.sort
+      (l2 + 0.05) / (l1 + 0.05)
+    end
+
+    # expects RGB as three numeric values between 0.0 and 255.0
+    # returns relative luminance of a color between 0.0 and 1.0
+    def relative_luminance(r, g, b)
       (0.2126 * f(r)) + (0.7152 * f(g)) + (0.0722 * f(b))
     end
 
+    # assists calculation of relative luminance
+    # expects a single RGB component between 0.0 and 255.0
     private def f(component)
       c = component / 255.0
       c <= 0.03928 ? c : ((c + 0.055) / 1.055) ** 2.4
--- test/luminosity_contrast_test.rb
+++ test/luminosity_contrast_test.rb
@@ -12,4 +12,8 @@ class LuminosityContrastTest < Minitest::Test
   def test_relative_luminance_of_white
     assert_equal 1.0, LuminosityContrast.relative_luminance(255,255,255)
   end
+
+  def test_ratio_of_black_and_white
+    assert_equal 21.0, LuminosityContrast.ratio([0,0,0], [255,255,255])
+  end
 end
