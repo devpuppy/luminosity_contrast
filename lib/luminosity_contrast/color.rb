41e0a19647d802ad069db827b8ffd40991010d70
Justin Jones <justin@devpuppy.com>
2016-04-26 19:49:51 -0400

    class Color parses RGB formats

---
+++ lib/luminosity_contrast/color.rb
@@ -0,0 +1,52 @@
+module LuminosityContrast
+  class Color
+    attr_reader :r, :g, :b
+
+    def initialize(*args)
+      input = args.size == 1 ? args[0] : args
+      @r, @g, @b = Color.rgb_from(input)
+    end
+
+    def relative_luminance
+      LuminosityContrast.relative_luminance(r,g,b)
+    end
+
+    def to_rgb
+      [r, g, b]
+    end
+
+    class << self
+
+      # expects a 3 or 6 character hex color code
+      # or an array with 3 numbers between 0 and 255
+      # or an object that responds to .r, .g, .b
+      def rgb_from(input)
+        case input
+        when String # hex code
+          raise 'Invalid hex code' unless input =~ /\A([0-9a-f]{3}){1,2}\z/i
+          rgb = if input.size == 3
+            input.split('').map { |h| h * 2 }
+          else
+            input.scan(/.{2}/)
+          end
+          rgb.map { |h| h.hex.to_i }
+        when Array # array of numbers
+          raise 'Invalid RGB array' unless input.size == 3
+          input.map(&:to_f)
+        when Hash
+          rgb = input.values_at(:r, :g, :b)
+          raise 'Invalid RGB hash' unless rgb.size == 3
+          rgb.map(&:to_f)
+        else
+          methods = [:r,:g, :b]
+          if methods.select { |m| input.respond_to?(m) }.size == methods.size
+            methods.map { |m| input.send(m) }
+          else
+            raise 'Invalid input'
+          end
+        end
+      end
+
+    end
+  end
+end
---
+++ test/color_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+require 'luminosity_contrast/color'
+
+class ColorTest < Minitest::Test
+  def test_color_from_hex_string_6
+    color = LuminosityContrast::Color.new('0000ff')
+    assert_blue(color)
+  end
+
+  def test_color_from_hex_string_3
+    color = LuminosityContrast::Color.new('00f')
+    assert_blue(color)
+  end
+
+  def test_color_from_array
+    color = LuminosityContrast::Color.new([0, 0, 255])
+    assert_blue(color)
+  end
+
+  def test_color_from_hash
+    color = LuminosityContrast::Color.new({b: 255, g: 0, r: 0})
+    assert_blue(color)
+  end
+
+  def test_color_from_positional_args
+    color = LuminosityContrast::Color.new(0, 0, 255)
+    assert_blue(color)
+  end
+
+  def test_color_from_kwargs
+    color = LuminosityContrast::Color.new(r: 0, g: 0, b: 255)
+    assert_blue(color)
+  end
+
+  def test_color_from_struct
+    Struct.new('MyColor', :r, :g, :b)
+    my_color = Struct::MyColor.new(0, 0, 255)
+    color = LuminosityContrast::Color.new(my_color)
+    assert_blue(color)
+  end
+
+  def test_color_from_color
+    original_color = LuminosityContrast::Color.new(0, 0, 255)
+    color = LuminosityContrast::Color.new(original_color)
+    assert_blue(color)
+  end
+
+  def assert_blue(color)
+    assert_equal [0.0, 0.0, 255.0], color.to_rgb, "Color is not blue: #{color.to_rgb}"
+  end
+end
